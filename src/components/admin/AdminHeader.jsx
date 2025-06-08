aac8bbe108ebcbeffab5cdb233a14cf8cef8c31f
Buzz-brain <chinomsochristian03@gmail.com>
2025-06-08 01:01:43 +0100

    Fix bugs and errors

--- src/components/admin/AdminHeader.jsx
+++ src/components/admin/AdminHeader.jsx
@@ -44,9 +44,9 @@ const AdminHeader = () => {
         </div>
 
         <div className="flex items-center space-x-4">
-          <button className="p-1.5 rounded-full text-gray-500 hover:text-gray-700 hover:bg-gray-100">
+          {/* <button className="p-1.5 rounded-full text-gray-500 hover:text-gray-700 hover:bg-gray-100">
             <Bell size={20} />
-          </button>
+          </button> */}
 
           <div className="relative">
             <button
